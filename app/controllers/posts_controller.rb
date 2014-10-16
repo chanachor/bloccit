3afd836f69ab820f7188dcf6e4bc8e5f173fced5
vagrant <vagrant@rails-dev-box>
2014-10-16 18:17:01 +0000

    refactored code to have method defined for reuse and update of params

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,7 +18,7 @@ class PostsController < ApplicationController
   def create
     #binding.pry
     @topic = Topic.find(params[:topic_id])
-    @post = current_user.posts.build(params.require(:post).permit(:title, :body)) 
+    @post = current_user.posts.build(post_params) 
     authorize @post
     if @post.save
       flash[:notice] = "Post was saved."
@@ -43,7 +43,7 @@ class PostsController < ApplicationController
     
     #@post.update_attributes(params.require[:post].permit(:title,:body))
 
-    if @post.update_attributes(params.require(:post).permit(:title,:body))
+    if @post.update_attributes(post_params)
       flash[:notice] = "Post was updated"
       redirect_to [@topic, @post]
     else
@@ -51,5 +51,11 @@ class PostsController < ApplicationController
       render :edit
     end
   
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
