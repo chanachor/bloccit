70cc1f834c73ff9435b8d5b283f1397f4f02b88c
vagrant <vagrant@rails-dev-box>
2014-10-17 03:57:16 +0000

    added private methods and create two render markdowns for posts and topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,19 @@
 class Topic < ActiveRecord::Base
   has_many :posts
+
+  def markdown_name
+    render_as_markdown name
+  end
+
+  def markdown_description
+    render_as_markdown description
+  end
+
+  private
+  def render_as_markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer,extensions)
+    (redcarpet.render text).html_safe
+  end
 end
